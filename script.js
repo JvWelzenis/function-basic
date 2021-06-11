8a22fe6d2d976ebf82f958e8991f4bb0d0e4aac0
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-06-11 20:52:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const makePizza = function () {
+console.log("maak deeg");
+console.log("maak saus");
+console.log("beleg pizza");
+console.log("kaas");
+console.log("doe pizza in de oven");
+};
+
+makePizza();
+
+const makeSushi = function () {
+console.log("kook rijst");
+console.log("leg rijst op vel");
+console.log("leg vis in vel");
+console.log("rol strak op");
+console.log("snij rol in stukjes");
+};
+
+makeSushi();
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
